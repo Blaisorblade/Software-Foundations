835faa708268ad9cbc837b25e8226e1ec7d3968b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-02-27 15:14:16 +0100

    Remove overcomplicated proof.

--- Basics.v
+++ Basics.v
@@ -1392,19 +1392,6 @@ Qed.
 Theorem evenb_n__oddb_Sn : forall n : nat,
   evenb n = negb (evenb (S n)).
 Proof.
-  intro n.
-  induction n as [| n'].
-
-  Case "n = 0".
-  reflexivity.
-
-  Case "n = S n'".
-  rewrite evenb_S_S.
-  rewrite IHn'.
-  rewrite negb_involutive.
-  reflexivity. (* Proof finished. *)
-  Restart.
-
   destruct n.
 
   Case "n = 0".
